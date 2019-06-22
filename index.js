8030e3c359ecab81d94834be9f9cd4b784bd8ee4
Aron Jon Prenovost <Aroniascoder@gmail.com>
2019-06-22 12:20:21 -0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-require('dotenv').config()
-const fs = require('fs')
-const Zillow = require('node-zillow')
+require('dotenv').config();
+const csvjson = require('csvjson');
+const readFile = require('fs').readFile;
+const writeFile = require('fs').writeFile;
+const Zillow = require('node-zillow');
 const zillow = new Zillow(process.env.ZILLOW_ID)
 
 const parameters = {
@@ -11,13 +13,31 @@ const parameters = {
 
 zillow.get('GetSearchResults', parameters)
     .then(results => {
-        results = JSON.stringify(results.response, null, 2);
 
-        fs.writeFile('test.txt', results, (err) => {
+        results = JSON.stringify(results.response.results.result, null, 2);
+        writeFile('test.json', results, (err) => {
             if (err) throw err;
-            console.log('The file has been saved!');
         });
-    })
+
+        // convert to csv
+        readFile('test.json', 'utf-8', (err, fileContent) => {
+            if (err) {
+                console.log(err);
+                throw new Error(err);
+            }
+            const csvData = csvjson.toCSV(fileContent, {
+                headers: 'key'
+            });
+            writeFile('test-data.csv', csvData, (err) => {
+                if (err) {
+                    console.log(err);
+                    throw new Error(err);
+                }
+                console.log('Success');
+            });
+        });
+    });
+
 
 
 
